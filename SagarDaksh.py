e79ff81b8e603b632f1b80bbb1048d2c38637cdc
Wasim <wewere4friends@gmail.com>
2022-06-14 11:13:03 -0800

    today is 14 june

---
+++ SagarDaksh.py
@@ -0,0 +1,4 @@
+print("this is about Sagar Daksh")
+print("Sagar Saini is Software Engg.")
+print("He lives in Shiv Vihar, Delhi")
+print("He is Unmaried ")
---
+++ SagarSaini.py
@@ -0,0 +1,4 @@
+print("this is about Sagar Saini")
+print("Sagar Saini is Software Engg.")
+print("He lives in Najafghad, Delhi")
+print("He is Maried and he has a daughter")
